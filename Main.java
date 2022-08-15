7c8b1197f36a3e8b0ec812b5aeaef4382aebe023
tharunvulava <90199079+tharunvulava@users.noreply.github.com>
2022-08-15 19:44:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ public class Main
 	public static void main(String[] args) 
 	{
 	   Queue <Integer> q=new LinkedList<Integer>(); 
-       q.add(30);                           
-       q.add(9);
-       q.add(2000);
-       q.add(8);
-       q.add(10);
-       q.add(2006);
-       System.out.println(q);
-       System.out.println(q.peek());
-       q.poll();
-       System.out.println(q.peek());
-       q.poll();
-       System.out.println(q);
+           q.add(30);                           
+           q.add(9);
+           q.add(2000);
+           q.add(8);
+           q.add(10);
+           q.add(2006);
+           System.out.println(q);
+           System.out.println(q.peek());
+           q.poll();
+           System.out.println(q.peek());
+           q.poll();
+           System.out.println(q);
 	}   
 }
